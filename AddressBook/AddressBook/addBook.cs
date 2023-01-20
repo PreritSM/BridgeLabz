6923ca5165755f982524b3cab3ec1e2164ebbfd7
Prerit Mittal <56505861+PreritSM@users.noreply.github.com>
2023-01-20 17:35:33 +0530

    Addressbook_stopwatch

---
+++ AddressBook/AddressBook/addBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AddressBook
+{
+    public class Contacts
+    {
+        string FName="";
+        string LName="";
+        string Address="";
+        string City="";
+        string State="";
+        int ZipCode;
+        long P_No;
+        string Email="";
+
+        public void AddContact(string FName, string LName, string Address, string City, string State, int ZipCode,long P_No,string Email)
+        {
+             this.FName = FName;
+             this.LName = LName;
+             this.Address = Address;
+             this.City = City;
+             this.State = State;
+             this.ZipCode = ZipCode;
+             this.P_No = P_No;
+             this.Email = Email;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{this.FName},{this.LName},{this.Address},{this.City},{this.State},{this.ZipCode},{this.P_No},{this.Email}");
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Stop_watch
+{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            Console.WriteLine("Please start the timer by an event");
+            Console.ReadKey();
+            stopwatch.Start();
+
+            Console.ReadKey();
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+
+            string elapsedTime = String.Format($"{ts.Hours}:{ts.Minutes}:{ts.Seconds}.{ts.Milliseconds}");
+            Console.WriteLine("RunTime " + elapsedTime);
+
+
+        }
+    }
+}
