538cd4811e3b23a0f6013257ffe98e06d58d2588
Prerit Mittal <56505861+PreritSM@users.noreply.github.com>
2023-01-22 01:31:28 +0530

    AddressBook UCs Done

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -6,8 +6,8 @@ namespace AddressBook
     {
         public static void Main(string []args) 
         {
+            IContactList contactList = new ContactList();
             Console.WriteLine("Welcome to the Address Book");
-            List <Contacts> ContactList = new List <Contacts> ();
             string FName = "Prerit";
             string LName = "Mittal";
             string Address = "Carnation";
@@ -18,17 +18,18 @@ namespace AddressBook
             string Email = "psmballer29@gmail.com";
 
             
-            Contacts ContactObj = new Contacts();
-            ContactObj.AddContact(FName, LName, Address, City, State, ZipCode, P_No, Email);
-            ContactList.Add(ContactObj);
-            Contacts ContactObj2= new Contacts();
-            ContactObj2.AddContact("Ayush", "Patel", "Wadhwa", "Kalyan", "Maharashtra", 421301, 8879572310, "lebronpatel04@gmail.com");
-            ContactList.Add(ContactObj2);
+            Contacts ContactObj = new Contacts(FName, LName, Address, City, State, ZipCode, P_No, Email);
+            Contacts ContactObj2= new Contacts("Ayush", "Patel", "Wadhwa", "Kalyan", "Maharashtra", 421301, 8879572310, "lebronpatel04@gmail.com");
+            contactList.Add_Contact(ContactObj);
+            contactList.Add_Contact(ContactObj2);
 
-            foreach (Contacts item in ContactList)
-            {
-                item.Display();
-            }
+            contactList.Display();
+
+            contactList.Edit_Contact("Ayush");
+
+            contactList.Delete_Contact("Prerit");
+
+            contactList.Display();
         }
     }
 }
--- AddressBook/AddressBook/addBook.cs
+++ AddressBook/AddressBook/addBook.cs
@@ -1,19 +1,31 @@
 ﻿using System;
+using System.Net.Http.Headers;
 
 namespace AddressBook
 {
+    public interface IContactList
+    {
+        public void Add_Contact(Contacts item);
+        public void Edit_Contact(string fname);
+        public void Delete_Contact(string fname);
+        public void Display();
+
+
+    }
+
+    
     public class Contacts
     {
-        string FName="";
-        string LName="";
-        string Address="";
-        string City="";
-        string State="";
-        int ZipCode;
-        long P_No;
-        string Email="";
+        public string FName="";
+        public string LName="";
+        public string Address="";
+        public string City="";
+        public string State="";
+        public int ZipCode;
+        public long P_No;
+        public string Email="";
 
-        public void AddContact(string FName, string LName, string Address, string City, string State, int ZipCode,long P_No,string Email)
+        public Contacts(string FName, string LName, string Address, string City, string State, int ZipCode,long P_No,string Email)
         {
              this.FName = FName;
              this.LName = LName;
@@ -28,6 +40,103 @@ namespace AddressBook
         public void Display()
         {
             Console.WriteLine($"{this.FName},{this.LName},{this.Address},{this.City},{this.State},{this.ZipCode},{this.P_No},{this.Email}");
+        }
+    }
+
+    public class ContactList : IContactList
+    {
+       // public List<string> Contact_list;
+        public Dictionary<string,Contacts> Add_Book;
+
+
+
+        public ContactList() {
+           // Contact_list = new List<string>();
+            Add_Book = new Dictionary<string, Contacts>();
+        }
+
+
+        public void Add_Contact(Contacts item)
+        {
+           // Contact_list.Add(item.FName);
+            Add_Book.Add(item.FName, item);
+        }
+
+        public void Edit_Contact(string fname) {
+
+            Console.WriteLine("Select Between the options you want to change for the given first name");
+            Console.WriteLine("1. Lastname");
+            Console.WriteLine("2. Address");
+            Console.WriteLine("3. City");
+            Console.WriteLine("4. State");
+            Console.WriteLine("5. Zipcode");
+            Console.WriteLine("6. Phone No");
+            Console.WriteLine("7. Email");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option-1)
+            {
+                case 0:
+                    Console.WriteLine("Please Enter The New Last Name");
+                    string name = Console.ReadLine();
+                    Add_Book[fname].LName = name;
+                    break;
+                case 1:
+                    Console.WriteLine("Please Enter The New Address");
+                    string name2 = Console.ReadLine();
+                    Add_Book[fname].Address = name2;
+                    break; 
+                case 2:
+                    Console.WriteLine("Please Enter The New City");
+                    string name3 = Console.ReadLine();
+                    Add_Book[fname].City = name3;
+                    break;
+                case 3:
+                    Console.WriteLine("Please Enter The New State");
+                    string name4 = Console.ReadLine();
+                    Add_Book[fname].State = name4;
+                    break;
+                case 4:
+                    Console.WriteLine("Please Enter The New ZipCode");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    Add_Book[fname].ZipCode = num;
+                    break;
+                case 5:
+                    Console.WriteLine("Please Enter the New Phone Number");
+                    long num2 = Convert.ToInt32(Console.ReadLine());
+                    Add_Book[fname].P_No= num2;
+                    break;
+                case 6:
+                    Console.WriteLine("Please Enter the New Email Id");
+                    string name5 = Console.ReadLine();
+                    Add_Book[fname].LName = name5;
+                    break;
+
+                default: 
+                    Console.WriteLine("Wrong Input");
+                    break;
+
+                    Console.WriteLine("\n");
+            }
+        }
+
+        public void Delete_Contact(string fname)
+        {
+            if (Add_Book.ContainsKey(fname))
+            {
+                Add_Book.Remove(fname);
+            }
+            else
+            { 
+                Console.WriteLine($"No contact exists with First name {fname}"); 
+            }
+        }
+        public void Display()
+        {
+            foreach (KeyValuePair <string,Contacts> item in Add_Book ) {
+                Console.WriteLine($"{Add_Book[item.Key].FName}\n{Add_Book[item.Key].LName}\n{Add_Book[item.Key].Address}\n" +
+                    $"{Add_Book[item.Key].City}\n{Add_Book[item.Key].State}\n{Add_Book[item.Key].ZipCode}\n" +
+                    $"{Add_Book[item.Key].P_No}\n{Add_Book[item.Key].Email}\n\n");
+            }
         }
     }
 }
