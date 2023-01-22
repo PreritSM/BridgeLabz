9d0492f242a094f8fcc2a1dd7d6fd9a1c94c7ba9
Prerit Mittal <56505861+PreritSM@users.noreply.github.com>
2023-01-22 19:55:46 +0530

    Generic Method

---
+++ GenericDemo/GenericDemo/GenericUC.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace GenericDemo
+{
+	public class GenericLearn
+	{
+        public void PrintArray<T>(T[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine($"The item displayed using Generic fn : {item}");
+            }
+        }
+    }
+	public class Generic_Max
+
+	{
+		public int Find_Max_Int(int a, int b, int c)
+		{
+		int max = a;
+		if (b > max)
+		{
+			max = b;
+		}
+		if (c > max)
+		{
+			max = c ;
+		}
+		return max;
+		}
+
+		public float Find_Max_Float(float a, float b, float c)
+		{
+			float max = a;
+			if (b > max)
+			{
+				max = b;
+			}
+			if (c > max)
+			{
+				max = c;
+			}
+			return max;
+		}
+
+		public string Find_Max_String(string a, string b, string c)
+		{
+			string max = a;
+			if (string.Compare(b,max)==1)
+			{
+				max = b;
+			}
+			if (string.Compare(c, max) == 1)
+			{
+				max = c;
+			}
+			return max;
+		}
+
+        public T Generic_Max_Find<T>(T[] arr)
+        {
+            var max = arr[0];
+            foreach (T t in arr)
+            {
+                if (Comparer<T>.Default.Compare(max, t) < 0)
+                {
+                    max = t;
+                }
+            }
+
+			return max;
+
+        }
+
+        public T Generic_Max_GenericVar<T>(T a, T b, T c)
+        {
+            if (Comparer<T>.Default.Compare(a, b) > 0 && Comparer<T>.Default.Compare(a, c) > 0)
+            {
+                return a;
+            }
+            else if (Comparer<T>.Default.Compare(b, a) > 0 && Comparer<T>.Default.Compare(b, c) > 0)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+
+        }
+    }
+}
--- GenericDemo/GenericDemo/Program.cs
+++ GenericDemo/GenericDemo/Program.cs
@@ -9,16 +9,25 @@ namespace GenericDemo
             int[] arr = { 1, 2, 3, 4, 5, 6 };
 
             char[] chars = { 'a', 'b', 'c' }; 
-            PrintArray<int>(arr);
-            PrintArray<char>(chars);
+            GenericLearn obj= new GenericLearn();
+            obj.PrintArray<int>(arr);
+            obj.PrintArray<char>(chars);
+
+            Generic_Max max_obj = new Generic_Max();
+            Console.WriteLine(max_obj.Find_Max_Int(1, 2, 3));
+            Console.WriteLine(max_obj.Find_Max_Float((float)1.5,(float) 2.5,(float) 3.5));
+            Console.WriteLine(max_obj.Find_Max_String("Apple", "Peach", "Banana"));
+
+
+            Console.WriteLine(max_obj.Generic_Max_GenericVar<string>("Apple","Mango","Orange"));
+
+            Console.WriteLine(max_obj.Generic_Max_Find<int>(arr));
+            Console.WriteLine(max_obj.Generic_Max_Find<char>(chars));
+
+
         }
 
-        public static void PrintArray<T>(T[] array)
-        {
-            foreach (var item in array)
-            {
-                Console.WriteLine($"The item displayed using Generic fn : {item}");
-            }
-        }
+
+
     }
 }
