a8b064d5be834b282e9f531f900ab43e14132eca
Prerit Mittal <56505861+PreritSM@users.noreply.github.com>
2023-02-20 00:09:05 +0530

    redis cache

---
+++ FundooApplication/FundooApplication/Middleware/CustomExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Automatonymous;
+using Microsoft.AspNetCore.Http;
+
+namespace FunDooApplication.Middleware
+{
+    public class CustomExceptionMiddleware
+    {
+        private readonly RequestDelegate requestDelegate;
+        public CustomExceptionMiddleware(RequestDelegate requestDelegate)
+        {
+            this.requestDelegate = requestDelegate;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await requestDelegate(httpContext);
+            }
+            catch (Exception ex)
+            {
+                var response = httpContext.Response;
+                response.ContentType = "application/json";
+                switch (ex)
+                {
+                    case KeyNotFoundException keyNotFound:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    case UnauthorizedAccessException unauthorizedAccessException:
+                        response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        break;
+                    //case BadRequestException badRequestException:
+                    //    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    //    break;
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ FundooApplication/RepositoryLayer/Helper/CustomException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace RepositoryLayer.Helpers
+{
+    public class CustomException : Exception
+    {
+        public ExceptionType type;
+        public enum ExceptionType { ObjectNullException, MethodNotFound, ClassNotFound }
+        public CustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
